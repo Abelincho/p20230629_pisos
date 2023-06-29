49acf325fe63945929d8900f432ef3c944cfe01c
Abel <you@example.com>
2023-06-29 16:51:06 +0200

    version 1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Superintendente Vicente");
+//console.log("Superintendente Vicente");
+
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  const pageIndex = 'https://www.idealista.com/venta-viviendas/leon-leon/mapa';
+  await page.goto(pageIndex);
+  // Haz lo que necesites con la página
+  await browser.close();
+})();
+
+
